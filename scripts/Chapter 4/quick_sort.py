7880ae6c071cd07dd7fe827fa7be9277966cae79
Aleksey Kouzmenko <aleksey.kouzmenko@gmail.com>
2020-09-22 16:58:07 +0400

    ch4 update quick sort

--- scripts/Chapter 4/quick_sort.py
+++ scripts/Chapter 4/quick_sort.py
@@ -3,10 +3,11 @@ from random import randrange as rnd
 def quickSort(arr):
     if len(arr) < 2:
         return arr
-    else:
-        pivot = arr[0]
-        less = [i for i in arr[1:] if i <= pivot]
-        grater = [i for i in arr[1:] if i > pivot]
+    else:        
+        j = rnd(len(arr)-1)
+        pivot = arr.pop(j)
+        less = [i for i in arr if i <= pivot]
+        grater = [i for i in arr if i > pivot]
         return quickSort(less) + [pivot] + quickSort(grater)
 
 def printArray(arr):
